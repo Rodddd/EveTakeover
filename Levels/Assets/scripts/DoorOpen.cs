1070fcfe709b83284f9e2e6d12319c763851e7ae
Rod <rodgonefishing@hotmail.co.uk>
2019-04-24 02:25:25 +0100

    More things completed for level 1
    Added a button for to open doors, added control clone etc.

---
+++ Levels/Assets/scripts/DoorOpen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+    public GameObject Door;
+    public bool doorIsOpening;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (doorIsOpening == true)
+        {
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+        if (Door.transform.position.y > 2.71f)
+        {
+            doorIsOpening = false;
+        }
+	}
+
+    void OnMouseDown()
+    {
+        doorIsOpening = true;
+    }
+}
